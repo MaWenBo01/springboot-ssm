b456f65ece367cc622c9108be3b34ece0893b9f3
MaWenBo01 <000MWBmwb>
2018-12-29 08:40:30 +0800

    ssm框架整理

---
+++ springboot-ssm-controller/src/main/java/com/mwb/springboot/ssm/sontroller/AppApplication.java
@@ -0,0 +1,23 @@
+package com.mwb.springboot.ssm.sontroller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+@ServletComponentScan
+@Slf4j
+public class AppApplication  {
+
+    public static void main(String[] args) {
+        try {
+            log.info("cpg_website 开始启动");
+            System.setProperty("server.port", "9900");
+            System.setProperty("spring.application.name", "cpg_website");
+         /*   BaseApplication.main(args);*/
+            log.info("cpg_website 已经启动");
+            log.info("本次启动的系统是cpg_website");
+        } catch (Exception e) {
+            log.error(e.getMessage(),e);
+        }
+    }
+}
---
+++ springboot-ssm-entity/src/main/java/com/mwb/springboot/ssm/entity/base/bean/User.java
@@ -0,0 +1,7 @@
+package com.mwb.springboot.ssm.entity.base.bean;
+
+/**
+ * Created by admin on 2018/12/28.
+ */
+public class User {
+}
